fcfce56c66b8ba34683ecefcb4fb88b4911ad30c
Akku-2325 <231696@astanait.edu.kz>
2025-02-14 09:33:40 +0500

    Basic middlewares with routes

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -2,38 +2,49 @@ package routes
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/DanikDaraboz/StoreProject/internal/handlers"
+	"github.com/DanikDaraboz/StoreProject/pkg/logger"
+	"github.com/DanikDaraboz/StoreProject/pkg/middleware"
 	"github.com/gorilla/mux"
 )
 
 func RegisterRoutes(s *handlers.Server) {
+	s.Router.Use(middleware.CORSMiddleware)
+	s.Router.Use(middleware.LoggerMiddleware)
+	s.Router.Use(middleware.RecoveryMiddleware)
 
-	// TODO middleware
+	// TODO 
+	// ClearCart, UpdateCartItemQuantity
 
+	// Public Routes
 	s.Router.HandleFunc("/", s.Home).Methods("GET")
-
-	s.Router.HandleFunc("/products", s.GetProducts).Methods("GET")           // List products
-	s.Router.HandleFunc("/products/{id}", s.GetProductByID).Methods("GET")   // Get product by ID
-	s.Router.HandleFunc("/products", s.CreateProduct).Methods("POST")        // Add product
-	s.Router.HandleFunc("/products/{id}", s.UpdateProduct).Methods("PUT")    // Update product
-	s.Router.HandleFunc("/products/{id}", s.DeleteProduct).Methods("DELETE") // Delete product
-
-	s.Router.HandleFunc("/orders", s.GetOrdersHandler).Methods("GET")           // List all orders
-	s.Router.HandleFunc("/orders/{id}", s.GetOrderByIDHandler).Methods("GET")   // Get order by ID
-	s.Router.HandleFunc("/orders", s.CreateOrderHandler).Methods("POST")        // Create new order
-	s.Router.HandleFunc("/orders/{id}", s.UpdateOrderHandler).Methods("PUT")    // Update order
-	s.Router.HandleFunc("/orders/{id}", s.DeleteOrderHandler).Methods("DELETE") // Delete order
-
-	// auth
-	// account
-
-	// wishlist
-	// cart
-	
 	s.Router.HandleFunc("/health", s.HealthCheckHandler).Methods("GET")
+	s.Router.HandleFunc("/products", s.GetProducts).Methods("GET")
+	s.Router.HandleFunc("/products/{id}", s.GetProductByID).Methods("GET")
+	s.Router.HandleFunc("/cart", s.AddCartItem).Methods("POST")        // Add item to the cart
+	s.Router.HandleFunc("/cart", s.GetCartItems).Methods("GET")           // View cart items
+	s.Router.HandleFunc("/cart/{id}", s.DeleteCartItem).Methods("DELETE") // Remove item from the cart
+	s.Router.HandleFunc("/login", s.LoginUser).Methods("POST")            // User login
+	s.Router.HandleFunc("/register", s.RegisterUser).Methods("POST")      // User registration
+	s.Router.HandleFunc("/logout", s.Logout).Methods("POST")              // User logout
 
-	fmt.Println("Registered routes:")
+	// Protected product routes (Admin-only)
+	s.Router.Handle("/products", middleware.AuthMiddleware(http.HandlerFunc(s.CreateProduct))).Methods("POST")        // Add a new product
+	s.Router.Handle("/products/{id}", middleware.AuthMiddleware(http.HandlerFunc(s.UpdateProduct))).Methods("PUT")    // Update a product
+	s.Router.Handle("/products/{id}", middleware.AuthMiddleware(http.HandlerFunc(s.DeleteProduct))).Methods("DELETE") // Delete a product
+
+	// Protected order routes (User-only)
+	s.Router.Handle("/orders", middleware.AuthMiddleware(http.HandlerFunc(s.CreateOrderHandler))).Methods("POST")        // Create a new order
+	s.Router.Handle("/orders", middleware.AuthMiddleware(http.HandlerFunc(s.GetOrdersHandler))).Methods("GET")           // Get all orders of user
+	s.Router.Handle("/orders/{id}", middleware.AuthMiddleware(http.HandlerFunc(s.GetOrderByIDHandler))).Methods("GET")   // Get details of the order
+	s.Router.Handle("/orders/{id}", middleware.AuthMiddleware(http.HandlerFunc(s.UpdateOrderHandler))).Methods("PUT")    // Update an order (status?)
+	s.Router.Handle("/orders/{id}", middleware.AuthMiddleware(http.HandlerFunc(s.DeleteOrderHandler))).Methods("DELETE") // Cancel an order
+	s.Router.Handle("/user", middleware.AuthMiddleware(http.HandlerFunc(s.GetUser))).Methods("GET")                       // View user profile
+	s.Router.Handle("/user", middleware.AuthMiddleware(http.HandlerFunc(s.UpdateUser))).Methods("PUT")                    // Update user profile
+
+	logger.InfoLogger.Println("Registered routes:")
 	s.Router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 		path, err := route.GetPathTemplate()
 		if err == nil {
--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -1,6 +1,10 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/DanikDaraboz/StoreProject/pkg/logger"
+)
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -13,3 +17,32 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func LoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger.WarnLogger.Printf("Request: %s %s", r.Method, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func RecoveryMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				logger.WarnLogger.Printf("Panic recovered: %v", err)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO validate session 
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// TODO Rate limit?
